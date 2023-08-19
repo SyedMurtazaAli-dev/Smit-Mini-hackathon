ba2880fcbc805b95bff3e17958071d8401c5ed0c
Syed Murtaza Ali <125219980+SyedMurtazaAli-dev@users.noreply.github.com>
2023-08-19 09:04:21 -0700

    update

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -14,7 +14,7 @@ export default function Dashboard() {
                                     id="bloghead"
                                     name="bloghead"
                                     type="text"
-                                    placeholder="   Place Holder"
+                                    placeholder="   Blog "
                                     required
                                     className="block w-4/5 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
