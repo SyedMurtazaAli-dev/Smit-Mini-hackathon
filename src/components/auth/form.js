bcf5214d4efc729713dd225cdc3a97a3ee331c68
Syed Murtaza Ali <125219980+SyedMurtazaAli-dev@users.noreply.github.com>
2023-08-19 07:56:36 -0700

    update

--- src/components/auth/form.js
+++ src/components/auth/form.js
@@ -20,6 +20,9 @@ export default function Form({ signin, onFormSubmit }) {
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           </h2>
         </div>
+   
+
+   
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" onSubmit={onSubmitHandler}  >
